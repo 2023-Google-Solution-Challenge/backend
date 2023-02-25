05f7e0e732d43c859896e8b9900d1fede931bed1
Yeonjeong Shin <87056545+shinyj1@users.noreply.github.com>
2023-02-25 22:07:19 +0900

    Initial commit

---
+++ src/main/java/com/example/signz/SignzApplication.java
@@ -0,0 +1,13 @@
+package com.example.signz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signz/SignzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
